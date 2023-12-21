402737590c8497b760f49aebe828c53bec3aa78c
VitorCarvalho67 <vitor.carvalho67@etec.sp.gov.br>
2023-12-21 20:30:29 -0300

    Refactor ViewAllBooksUseCase to exclude author ID and include author name in the response

--- src/modules/books/useCases/ViewAllBooks/ViewAllBooksUseCase.ts
+++ src/modules/books/useCases/ViewAllBooks/ViewAllBooksUseCase.ts
@@ -3,17 +3,42 @@ import { prisma } from "../../../../prisma/client";
 import { AppError } from "../../../../errors/error";
 
 export class ViewAllBooksUseCase {
+    // async execute(): Promise<Book[]> {
+    //     const books = await prisma.book.findMany({
+    //         include: {
+    //             author: true
+    //         }
+    //     }
+    //     );
+
+    //     if (!books) {
+    //         throw new AppError("No books found");
+    //     }
+    //     return books;
+    // }
+
+    // Sem mostrar o autor id só o nome do autor e sem o id na resposta do json
     async execute(): Promise<Book[]> {
         const books = await prisma.book.findMany({
+            
             include: {
-                author: true
+                author: {
+                    select: {
+                        name: true
+                    }
+                }
             }
-        }
-        );
-
+        });
+        
         if (!books) {
             throw new AppError("No books found");
         }
-        return books;
+
+        return books.map(book => {
+            return {
+                ...book,
+            }
+        });
     }
+
 }
