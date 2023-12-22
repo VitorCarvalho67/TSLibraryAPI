f3c98d35abbcf431ca9ad66ecfffff490e92d70b
VitorCarvalho67 <vitor.carvalho67@etec.sp.gov.br>
2023-12-21 21:05:13 -0300

    Add Avaliation and Devolution models
    Update ViewAllBooksUseCase

--- src/modules/books/useCases/ViewAllBooks/ViewAllBooksUseCase.ts
+++ src/modules/books/useCases/ViewAllBooks/ViewAllBooksUseCase.ts
@@ -3,21 +3,6 @@ import { prisma } from "../../../../prisma/client";
 import { AppError } from "../../../../errors/error";
 
 export class ViewAllBooksUseCase {
-    // async execute(): Promise<Book[]> {
-    //     const books = await prisma.book.findMany({
-    //         include: {
-    //             author: true
-    //         }
-    //     }
-    //     );
-
-    //     if (!books) {
-    //         throw new AppError("No books found");
-    //     }
-    //     return books;
-    // }
-
-    // Sem mostrar o autor id só o nome do autor e sem o id na resposta do json
     async execute(): Promise<Book[]> {
         const books = await prisma.book.findMany({
             
